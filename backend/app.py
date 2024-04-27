36cbf3a0d379ac2760e080670753ae7c66b671b6
Yehdar <p.radhey06@gmail.com>
2024-04-27 11:10:42 -0400

    backend: add JSON serialization and initialize db

--- backend/app.py
+++ backend/app.py
@@ -13,3 +13,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def json(self):
+        return {'id': self.id,'username': self.username, 'email': self.email}
+
+db.create_all()
