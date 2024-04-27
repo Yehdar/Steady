df629bb39692717630f2bbdbe92fe3d188afba53
Yehdar <p.radhey06@gmail.com>
2024-04-27 11:07:21 -0400

    feat: define user model for db

--- backend/app.py
+++ backend/app.py
@@ -7,3 +7,9 @@ app - Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
